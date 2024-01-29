cf62f2a79a8cfa1e8c63f29d56abc97b68d62ab4
RMLemos <143395332+RMLemos@users.noreply.github.com>
2024-01-29 16:45:50 +0000

    search form

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name = 'backoffice'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
--- backoffice/views/contact_views.py
+++ backoffice/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 from backoffice.models import Contact
 
 def index(request):
@@ -6,10 +7,54 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contact Board'
     }
 
     return render(
         request,
         'backoffice/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('backoffice:index')
+
+    contacts = Contact.objects \
+        .filter(status=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('first_name')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request,
+        'backoffice/index.html',
+        context
+    )
+
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact, pk=contact_id, status=True)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'backoffice/contact.html',
         context
     )
