d1b4e8baa40de18e983e67d5541f38ffcc671a9c
RMLemos <143395332+RMLemos@users.noreply.github.com>
2024-01-31 17:43:59 +0000

    send email config

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
 from landingpage.forms import EbookForm
+from django.core.mail import send_mail
 
 def home(request):
     if request.method == 'POST':
         form = EbookForm(request.POST)
         if form.is_valid():
             form.save()
+            send_mail(
+            "Subject here",
+            "Here is the message.",
+            "from@example.com",
+            ["to@example.com"],
+            fail_silently=False,
+            )
             return redirect('landingpage:sent')
     else:
         form = EbookForm()
